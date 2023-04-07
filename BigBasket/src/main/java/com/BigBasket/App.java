a1635148f38f65d6e4546219175745c5a8d94c4c
lalit3619 <lakshmikant633@gmail.com>
2023-04-08 00:59:37 +0530

    maven project

---
+++ BigBasket/src/main/java/com/BigBasket/App.java
@@ -0,0 +1,13 @@
+package com.BigBasket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
